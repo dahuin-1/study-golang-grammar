44adc090adcdf9e21749407e4281b1c2380d1f78
dahuin-1 <dawhite01@naver.com>
2022-01-23 01:57:15 +0900

    add test code

---
+++ 13/test.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type student struct {
+	...
+}
+
+
+
+func main() {
+	var ... int
+	var ... string
+	
+	fmt.Scanln()
+	
+	
+	
+	for  {	
+		fmt.Scanln()
+		
+		
+		
+		
+		for  {
+			fmt.Scanln()
+
+			
+		}
+		
+	}
+	
+	for  {
+		fmt.Println("----------")
+		
+		
+		for index, val := range s[i].score {
+			fmt.Println(index, val)
+		}
+		
+	}
+	fmt.Println("----------")
+}
