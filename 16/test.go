ab4c270a689f11ef8c12f9392d6e745c48f948f8
dahuin-1 <dawhite01@naver.com>
2022-01-25 01:22:19 +0900

    add test.go

---
+++ 16/test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    //"log"
+)
+
+func errorChek(score int) (int, error) {
+    if score >= 0 {
+        return score, nil
+    }
+    return 0, fmt.Errorf("시험 점수를 양의 정수로 입력하세요.")
+}
+
+func main() {
+    var score int
+    fmt.Scanln(&score)
+    
+    s, err := errorChek(score)
+    
+    if err != nil {
+        //log.Panic(err)
+		fmt.Println("testtest")
+    }
+    fmt.Printf("시험 점수는 %d점입니다.",s)
+}
