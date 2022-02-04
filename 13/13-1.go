8e43cb94313459e90843778354d0b7792b72997f
dahuin-1 <dawhite01@naver.com>
2022-02-04 18:04:03 +0900

    re test

--- 13/13-1.go
+++ 13/13-1.go
@@ -3,46 +3,42 @@ package main
 import "fmt"
 
 type student struct {
-	/* 
-	학생 이름 정보를 담는 string형 변수, 
-	학생 성별 정보를 담는 string형 변수, 
-	key 값으로 과목 이름, value 값으로 점수를 담는 맵형 컬렉션
-	*/
-	name 	string
-	gender 	string
-	data 	map[string]int
+	name string
+	gender string
+	data map[string]int
 }
-//생성자함수
-func newStudent() *student { //포인터 구조체를 반환함
-	s := student{} //구조체 객체를 생성하고 초기화함
-	s.data = map[string]int{}
-	return &s //초기화 한 포인터 구조체를 반환함
+
+func newStudent() *student {
+	d := student {}
+	d.data = map[string]int{}
+	return &d
 }
 
 func main() {
-	var studentNum, subjectNum, score int
+	var numOfStudent, numOfSubject, score int
 	var subjectName, name, gender string
 	
-	fmt.Scanln(&studentNum, &subjectNum)
+	fmt.Scanln(&numOfStudent, &numOfSubject)
 	
-	var studentList = make([]student, studentNum)
+	var studentList = make([]student, numOfStudent)
 	
-	for i :=0; i < studentNum; i++ { 
+	for i := 0; i < numOfStudent; i++  {	
 		fmt.Scanln(&name, &gender)
-		s1 := newStudent() //생성자 호출을 통한 student 객체 생성
+
+		s1 := newStudent() //student 객체 생성
 
 		s1.name = name
 		s1.gender = gender
 		
-		for j :=0; j < subjectNum; j++ {
+		for j := 0; j < numOfSubject; j++ {
 			fmt.Scanln(&subjectName, &score)
-
+            
 			s1.data[subjectName] = score
 		}
 		studentList[i] = *s1
 	}
 	
-	for i :=0; i < studentNum; i++ { 
+	for i := 0; i < numOfStudent; i++  {
 		fmt.Println("----------")
 		fmt.Println(studentList[i].name, studentList[i].gender)
 		
@@ -52,5 +48,4 @@ func main() {
 		
 	}
 	fmt.Println("----------")
-	
 }
