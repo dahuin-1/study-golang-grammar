c626355af18928259498a9122d71066eef72c962
dahuin-1 <dawhite01@naver.com>
2022-02-05 23:27:39 +0900

    add 14 package

---
+++ 14/14-1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+
+
+
+
+
+
+
+
+func main() {
+	
+
+	cy1 :=
+	cy2 :=
+	cu1 :=
+	cu2 :=
+	
+	printMeasure(cy1, cy2, cu1, cu2)	
+}
+
+func printMeasure() {
+	
+}
